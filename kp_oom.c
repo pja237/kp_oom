9fdd8a118cd1722bbbca41732691575298fd2a71
Jager,Petar <petar.jager@gmail.com>
2021-05-17 12:14:02 +0200

    working av_return_0 code

--- kp_oom.c
+++ kp_oom.c
@@ -58,51 +58,186 @@ unsigned long DFS(struct task_struct *task)
 
 int kp_pre(struct kprobe *k, struct pt_regs *r)
 {
-    int count_sing = 0;
-    long int pc_pages;
+//    int count_sing = 0;
+//    long int pc_pages;
+//    struct task_struct *tmp_ts;
+//    const struct cred *cred = current_cred();
+//    struct mm_struct *mm; 
+//    // --------------------------------------------------------------------------------
+//    // eventfd vars
+//    // --------------------------------------------------------------------------------
+//    struct file * efd_file = NULL;          //...to eventfd's file struct
+//    struct eventfd_ctx * efd_ctx = NULL;        //...and finally to eventfd context
+//    uint64_t plus_one = 1;
+//    // --------------------------------------------------------------------------------
+//    struct cgroup *cg;
+//    struct mem_cgroup *memcg;
+//    unsigned long total_rss=0;
+//
+//    //if(current==NULL) return 0;
+      pr_debug("KPROBE PRE-FIRE on %s from pid=%d!\n", KALLSYM, current->pid);
+//
+//    // if we're an exiting slurmstepd, don't do anything.... abort this path!
+//    if(strncmp(current->comm, SLURM, sizeof(SLURM)) == 0) {
+//        pr_debug(" Exiting slurmstepd, ignore.\n");
+//        return 0;
+//    }
+//
+//    // else do work...
+//    //dump_stack();
+//    mm=get_task_mm(current);
+//    cg=task_cgroup(current, mem_cgroup_subsys_id);
+//    memcg=(struct mem_cgroup *) cg->subsys[mem_cgroup_subsys_id];
+//
+//    pr_debug("--------------------------------------------------------------------------------\n");
+//    pr_debug("mm.rss pid(%d) = %ld !\n", current->pid, get_mm_rss(mm));
+//    pr_debug("mm.total_vm pid(%d) = %ld !\n", current->pid, mm->total_vm);
+//    pr_debug("mm.hiwater_rss pid(%d) = %ld !\n", current->pid, mm->hiwater_rss);
+//    pr_debug("mm.locked_vm pid(%d) = %ld !\n", current->pid, mm->locked_vm);
+//    pr_debug("mem_cgroup pid(%d) memcg * = %p  usage = %lu limit= %lu watermark= %lu failcnt= %lu\n", current->pid, memcg, page_counter_read(&memcg->memory), memcg->memory.parent->limit, memcg->memory.watermark, memcg->memory.failcnt);
+//    pr_debug("mem_cgroup pid(%d) kmem=%lu\n", current->pid, page_counter_read(&memcg->kmem));
+//    pr_debug("--------------------------------------------------------------------------------\n");
+//
+//    // if this is called from somewhere that is not a descendant of slurmstepd, also abort!
+//    tmp_ts=current;
+//    if(tmp_ts->parent==NULL || tmp_ts->parent==tmp_ts) {
+//        pr_alert("parent killed!!!\n");
+//        return 0;
+//    }
+//    while(tmp_ts->parent!=tmp_ts && tmp_ts->pid != 1 && strncmp(tmp_ts->parent->comm, SLURM, sizeof(SLURM)) != 0) {
+//        pr_debug("WALK UP tmp_ts pid=%d comm=%s\n", tmp_ts->pid, tmp_ts->comm);
+//        pr_debug("WALK ABOVE tmp_ts->parent->pid=%d parent->comm=%s\n", tmp_ts->parent->pid, tmp_ts->parent->comm);
+//        if(strncmp(tmp_ts->comm, SINGULARITY, sizeof(SINGULARITY)) == 0) {
+//            count_sing++;
+//        }
+//        if(tmp_ts->parent==NULL) {
+//            pr_alert("parent killed!!!\n");
+//            return 0;
+//        }
+//        tmp_ts=tmp_ts->parent;
+//    }
+//    if(strncmp(tmp_ts->comm, SINGULARITY, sizeof(SINGULARITY)) == 0) {
+//        count_sing++;
+//    }
+//    pr_debug("WALK TOP pid=%d comm=%s count_sing=%d\n", tmp_ts->pid, tmp_ts->comm, count_sing);
+//    if(tmp_ts->pid == 1 || count_sing == 0) {
+//        // we have walked all the way up to the top, so we didn't come from slurm => abort!
+//        // OR we haven't encountered singularity starter-suid above us, also abort!
+//        pr_debug("WALK TOP shows we're no descendant of slurmstepd or singularity, abort!\n");
+//        return 0;
+//    }
+//    // here tmp_ts is pointing to the 1st descendant of slurmstepd, meaning... 
+//    // ...we could try to terminate that one
+//    // ...also tmp_ts->parent is pointing to slurmstepd which we need for eventfd below! Excellent!
+//
+//    // Here we sum up the memcg total_rss to compare it to threshold
+//    pr_alert("pre-DFS: CURRENT(%d) flags= %u state= %ld\n", current->pid, tmp_ts->flags, tmp_ts->state);
+//    if(!tmp_ts) {
+//	pr_alert("ABORT!\n");
+//	return 0;
+//    }
+//    if(list_empty(&tmp_ts->parent->children)) {
+//        pr_alert("pre-DFS: CURRENT(%d) tmp_ts->parent->children empty children list, return 0", current->pid);
+//        return 0;
+//    }
+//    total_rss=DFS(tmp_ts->parent);
+//    if(total_rss==0) {
+//	pr_alert("total_rss==0!!! ABORT\n");
+//	return 0;
+//    }
+//    pr_debug("DFS-RESULT: CURRENT(%d) total_rss = %lu\n", current->pid, total_rss);
+//
+//    // if more then that is taken by the PC, don't kill anything, resume operations...
+//    pc_pages=100*total_rss/memcg->memory.parent->limit;
+//    pr_debug("pc_pages = %ld\n", pc_pages);
+//    if(pc_pages < 100-bail_mark_percent) {
+//        pr_debug("Still not under too much pressure, resuming...\n");
+//        return 0;
+//    }
+//
+//    pr_alert("Under %d %% pagecache left in memcg, abort!\n", bail_mark_percent);
+//
+//    // --------------------------------------------------------------------------------
+//    // This eventfd snippet comes from https://stackoverflow.com/questions/13607730/writing-to-eventfd-from-kernel-module
+//    // --------------------------------------------------------------------------------
+//    pr_debug("tmp_ts->parent pid=%d comm=%s\n", tmp_ts->parent->pid, tmp_ts->parent->comm);
+//    // ok, we're here, lets try to send an event
+//    rcu_read_lock();
+//    // slurm efd = 12 for .batch and .extern (those don't have fd up to 17)
+//    //           = 17 for .0
+//    efd_file = fcheck_files(tmp_ts->parent->files, 17);
+//    if(efd_file == NULL) {
+//        pr_debug("edf fd 17 failed, we're in .batch or .extern, trying for 12\n");
+//        efd_file = fcheck_files(tmp_ts->parent->files, 12);
+//        if(efd_file != NULL) {
+//            pr_debug("edf fd 12 success!\n");
+//        }
+//        else {
+//            pr_alert("Could not find eventfd file, aborting!\n");
+//            return 0;
+//        }
+//    }
+//    rcu_read_unlock();
+//    efd_ctx = eventfd_ctx_fileget(efd_file);
+//    if (!efd_ctx) {
+//        pr_debug("eventfd_ctx_fileget() FAILED\n");
+//        // uh-oh.... dragons ahead!?!
+//        return -1;
+//    }
+//    pr_debug("Resolved pointer to the userspace program's eventfd's context: %p \n", efd_ctx);
+//
+//    eventfd_signal(efd_ctx, plus_one);
+//
+//    pr_debug("Incremented userspace program's eventfd's counter by 1\n");
+//
+//    eventfd_ctx_put(efd_ctx);
+//    // --------------------------------------------------------------------------------
+//    // EOSTEAL
+//    // --------------------------------------------------------------------------------
+//    //
+//    // Now we terminate the task (current)...
+//    // OR... terminate 1st descendant of slurmstepd (tmp_ts) and by that terminate the job itself
+//    // OR BOTH!?
+//    // force_sig(9, current);
+//    // send_sig(9, current, 0);
+//    //
+//    // pr_alert("Call send_sig(SIGKILL) on pid=%d comm=%s\n", tmp_ts->pid, tmp_ts->comm);
+//    pr_alert("Call send_sig(SIGKILL) on pid=%d comm=%s parent.comm=%s uid=%d\n", tmp_ts->pid, tmp_ts->comm, tmp_ts->parent->comm, cred->uid.val);
+//    //send_sig(9, tmp_ts, 0);
+//    //send_sig(9, current, 0);
+//    //kill_pid(find_vpid(tmp_ts->pid), 9, 0);
+//    rcu_read_lock();
+//    //kill_pid(find_vpid(tmp_ts->pid), 9, 0);
+//    pr_alert("post-kill_pid: flags= %u state= %ld\n", tmp_ts->flags, tmp_ts->state);
+//    mdelay(1000);
+//    rcu_read_unlock();
+//    pr_alert("...call finished\n");
+    return 0;
+}
+
+void kp_post(struct kprobe *k, struct pt_regs *r, unsigned long flags)
+{
     struct task_struct *tmp_ts;
-    const struct cred *cred = current_cred();
-    struct mm_struct *mm; 
-    // --------------------------------------------------------------------------------
-    // eventfd vars
-    // --------------------------------------------------------------------------------
-    struct file * efd_file = NULL;          //...to eventfd's file struct
-    struct eventfd_ctx * efd_ctx = NULL;        //...and finally to eventfd context
-    uint64_t plus_one = 1;
-    // --------------------------------------------------------------------------------
-    struct cgroup *cg;
-    struct mem_cgroup *memcg;
     unsigned long total_rss=0;
+    long int pc_pages;
+    int count_sing = 0;
+    struct mem_cgroup *memcg;
+    struct cgroup *cg;
+    struct mm_struct *mm; 
 
-    //if(current==NULL) return 0;
-    pr_debug("KPROBE PRE-FIRE on %s from pid=%d!\n", KALLSYM, current->pid);
+    int retval = regs_return_value(r);
 
-    // if we're an exiting slurmstepd, don't do anything.... abort this path!
-    if(strncmp(current->comm, SLURM, sizeof(SLURM)) == 0) {
-        pr_debug(" Exiting slurmstepd, ignore.\n");
-        return 0;
-    }
+    pr_alert("%s returned %d\n", KALLSYM, retval);
 
-    // else do work...
-    //dump_stack();
     mm=get_task_mm(current);
     cg=task_cgroup(current, mem_cgroup_subsys_id);
     memcg=(struct mem_cgroup *) cg->subsys[mem_cgroup_subsys_id];
 
-    pr_debug("--------------------------------------------------------------------------------\n");
-    pr_debug("mm.rss pid(%d) = %ld !\n", current->pid, get_mm_rss(mm));
-    pr_debug("mm.total_vm pid(%d) = %ld !\n", current->pid, mm->total_vm);
-    pr_debug("mm.hiwater_rss pid(%d) = %ld !\n", current->pid, mm->hiwater_rss);
-    pr_debug("mm.locked_vm pid(%d) = %ld !\n", current->pid, mm->locked_vm);
-    pr_debug("mem_cgroup pid(%d) memcg * = %p  usage = %lu limit= %lu watermark= %lu failcnt= %lu\n", current->pid, memcg, page_counter_read(&memcg->memory), memcg->memory.parent->limit, memcg->memory.watermark, memcg->memory.failcnt);
-    pr_debug("mem_cgroup pid(%d) kmem=%lu\n", current->pid, page_counter_read(&memcg->kmem));
-    pr_debug("--------------------------------------------------------------------------------\n");
-
     // if this is called from somewhere that is not a descendant of slurmstepd, also abort!
     tmp_ts=current;
-    if(tmp_ts->parent==NULL || tmp_ts->parent==tmp_ts) {
+    if(tmp_ts->parent==tmp_ts) {
         pr_alert("parent killed!!!\n");
-        return 0;
+        return;
     }
     while(tmp_ts->parent!=tmp_ts && tmp_ts->pid != 1 && strncmp(tmp_ts->parent->comm, SLURM, sizeof(SLURM)) != 0) {
         pr_debug("WALK UP tmp_ts pid=%d comm=%s\n", tmp_ts->pid, tmp_ts->comm);
@@ -112,7 +247,7 @@ int kp_pre(struct kprobe *k, struct pt_regs *r)
         }
         if(tmp_ts->parent==NULL) {
             pr_alert("parent killed!!!\n");
-            return 0;
+            return;
         }
         tmp_ts=tmp_ts->parent;
     }
@@ -124,26 +259,27 @@ int kp_pre(struct kprobe *k, struct pt_regs *r)
         // we have walked all the way up to the top, so we didn't come from slurm => abort!
         // OR we haven't encountered singularity starter-suid above us, also abort!
         pr_debug("WALK TOP shows we're no descendant of slurmstepd or singularity, abort!\n");
-        return 0;
+        return;
     }
     // here tmp_ts is pointing to the 1st descendant of slurmstepd, meaning... 
     // ...we could try to terminate that one
     // ...also tmp_ts->parent is pointing to slurmstepd which we need for eventfd below! Excellent!
 
+    //
     // Here we sum up the memcg total_rss to compare it to threshold
-    pr_alert("pre-DFS: CURRENT(%d) flags= %u state= %ld\n", current->pid, tmp_ts->flags, tmp_ts->state);
+    pr_alert("post-handler: CURRENT(%d) flags= %u state= %ld\n", current->pid, tmp_ts->flags, tmp_ts->state);
     if(!tmp_ts) {
 	pr_alert("ABORT!\n");
-	return 0;
+	return;
     }
     if(list_empty(&tmp_ts->parent->children)) {
         pr_alert("pre-DFS: CURRENT(%d) tmp_ts->parent->children empty children list, return 0", current->pid);
-        return 0;
+        return;
     }
     total_rss=DFS(tmp_ts->parent);
     if(total_rss==0) {
 	pr_alert("total_rss==0!!! ABORT\n");
-	return 0;
+	return;
     }
     pr_debug("DFS-RESULT: CURRENT(%d) total_rss = %lu\n", current->pid, total_rss);
 
@@ -152,71 +288,13 @@ int kp_pre(struct kprobe *k, struct pt_regs *r)
     pr_debug("pc_pages = %ld\n", pc_pages);
     if(pc_pages < 100-bail_mark_percent) {
         pr_debug("Still not under too much pressure, resuming...\n");
-        return 0;
+        return;
     }
 
     pr_alert("Under %d %% pagecache left in memcg, abort!\n", bail_mark_percent);
+    pr_alert("Modifying %s retval to 0\n", KALLSYM);
+    regs_set_return_value(r, 0ul);
 
-    // --------------------------------------------------------------------------------
-    // This eventfd snippet comes from https://stackoverflow.com/questions/13607730/writing-to-eventfd-from-kernel-module
-    // --------------------------------------------------------------------------------
-    pr_debug("tmp_ts->parent pid=%d comm=%s\n", tmp_ts->parent->pid, tmp_ts->parent->comm);
-    // ok, we're here, lets try to send an event
-    rcu_read_lock();
-    // slurm efd = 12 for .batch and .extern (those don't have fd up to 17)
-    //           = 17 for .0
-    efd_file = fcheck_files(tmp_ts->parent->files, 17);
-    if(efd_file == NULL) {
-        pr_debug("edf fd 17 failed, we're in .batch or .extern, trying for 12\n");
-        efd_file = fcheck_files(tmp_ts->parent->files, 12);
-        if(efd_file != NULL) {
-            pr_debug("edf fd 12 success!\n");
-        }
-        else {
-            pr_alert("Could not find eventfd file, aborting!\n");
-            return 0;
-        }
-    }
-    rcu_read_unlock();
-    efd_ctx = eventfd_ctx_fileget(efd_file);
-    if (!efd_ctx) {
-        pr_debug("eventfd_ctx_fileget() FAILED\n");
-        // uh-oh.... dragons ahead!?!
-        return -1;
-    }
-    pr_debug("Resolved pointer to the userspace program's eventfd's context: %p \n", efd_ctx);
-
-    eventfd_signal(efd_ctx, plus_one);
-
-    pr_debug("Incremented userspace program's eventfd's counter by 1\n");
-
-    eventfd_ctx_put(efd_ctx);
-    // --------------------------------------------------------------------------------
-    // EOSTEAL
-    // --------------------------------------------------------------------------------
-    //
-    // Now we terminate the task (current)...
-    // OR... terminate 1st descendant of slurmstepd (tmp_ts) and by that terminate the job itself
-    // OR BOTH!?
-    // force_sig(9, current);
-    // send_sig(9, current, 0);
-    //
-    // pr_alert("Call send_sig(SIGKILL) on pid=%d comm=%s\n", tmp_ts->pid, tmp_ts->comm);
-    pr_alert("Call send_sig(SIGKILL) on pid=%d comm=%s parent.comm=%s uid=%d\n", tmp_ts->pid, tmp_ts->comm, tmp_ts->parent->comm, cred->uid.val);
-    //send_sig(9, tmp_ts, 0);
-    //send_sig(9, current, 0);
-    //kill_pid(find_vpid(tmp_ts->pid), 9, 0);
-    rcu_read_lock();
-    kill_pid(find_vpid(tmp_ts->pid), 9, 0);
-    pr_alert("post-kill_pid: flags= %u state= %ld\n", tmp_ts->flags, tmp_ts->state);
-    mdelay(1000);
-    rcu_read_unlock();
-    pr_alert("...call finished\n");
-    return 0;
-}
-
-void kp_post(struct kprobe *k, struct pt_regs *r, unsigned long flags)
-{
     pr_debug("kprobe post-FIRE on %s!\n", KALLSYM);
     return;
 }
